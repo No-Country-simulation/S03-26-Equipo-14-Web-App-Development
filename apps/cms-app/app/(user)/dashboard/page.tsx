c4bfe3bc75622a1371154d7a75046ba818c9a41a
Gabriela Díaz <gldt2494@gmail.com>
2026-04-02 18:25:02 -0500

    feat: add ErrorMessage component

--- apps/cms-app/app/(user)/dashboard/page.tsx
+++ apps/cms-app/app/(user)/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { EmptyDashboard } from './_components/empty-dashboard';
+import { ErrorMessage } from './_components/error-message';
 
 export default function DashboardPage() {
-  return <EmptyDashboard />;
+  // return <EmptyDashboard />;
+  return <ErrorMessage />;
 }
