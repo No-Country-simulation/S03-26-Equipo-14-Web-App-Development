2ad84ef598dcf6cab1e9e580eb76b0706236e780
Gabriela Díaz <gldt2494@gmail.com>
2026-04-02 18:51:24 -0500

    feat: add NoProjects component

--- apps/cms-app/app/(user)/dashboard/page.tsx
+++ apps/cms-app/app/(user)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { EmptyDashboard } from './_components/empty-dashboard';
 import { ErrorMessage } from './_components/error-message';
+import { NoProjects } from './_components/no-projects';
 
 export default function DashboardPage() {
-  // return <EmptyDashboard />;
-  return <ErrorMessage />;
+  return <EmptyDashboard />;
+  // return <ErrorMessage />;
+  // return <NoProjects />;
 }
