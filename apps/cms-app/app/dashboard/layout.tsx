3f1101e6b9ef0ff23fc1bfc15ac85e5bda4029a0
Gabriela Díaz <gldt2494@gmail.com>
2026-04-02 01:45:03 -0500

    feat: add sidebar and header to dashboard layout

--- apps/cms-app/app/dashboard/layout.tsx
+++ apps/cms-app/app/dashboard/layout.tsx
@@ -2,13 +2,32 @@ import { authConfig } from '@/auth.config';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset,
+} from '@repo/ui/components';
+import { AppSidebar } from './_components/sidebar-dashboard';
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode; }) {
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const session = await getServerSession(authConfig);
   if (!session) {
     redirect('/auth/login');
   }
   return (
-    <>{children}</>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex flex-row">
+          <SidebarTrigger />
+          <h1>Título</h1>
+        </header>
+        <main className="flex flex-col">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
