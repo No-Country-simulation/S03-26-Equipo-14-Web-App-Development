387be6d44efa9cced686fe14601d0908bd6c3a21
LautaroLD <duranlautarogabriel@gmail.com>
2026-04-03 16:47:25 -0300

    feat: add create testimonial form and file upload hooks

---
+++ apps/cms-app/app/dashboard/testimonials/create/page.tsx
@@ -0,0 +1,5 @@
+import { CreateTestimonialForm } from './_components/create-testimonial-form';
+
+export default function CreateTestimonialPage() {
+  return <CreateTestimonialForm mode="create" />;
+}
