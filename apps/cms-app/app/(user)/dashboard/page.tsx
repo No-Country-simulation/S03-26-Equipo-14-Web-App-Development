dcefca8c00b15aba7226f3962b8c18cf3c828bed
Gabriela Díaz <gldt2494@gmail.com>
2026-04-04 23:51:50 -0500

    feat: implement FiltersBar in DashboardPage

--- apps/cms-app/app/(user)/dashboard/page.tsx
+++ apps/cms-app/app/(user)/dashboard/page.tsx
@@ -2,9 +2,15 @@ import { EmptyDashboard } from './_components/empty-dashboard';
 import { ErrorMessage } from './_components/error-message';
 import { NoProjects } from './_components/no-projects';
 import { Loading } from './_components/loading';
+import { FiltersBar } from './_components/filtersbar';
 
 export default function DashboardPage() {
-  return <EmptyDashboard />;
+  return (
+    <section className="flex flex-col gap-4 h-full">
+      <FiltersBar />
+      <EmptyDashboard />
+    </section>
+  );
   // return <ErrorMessage />;
   // return <NoProjects />;
   // return <Loading/>;
