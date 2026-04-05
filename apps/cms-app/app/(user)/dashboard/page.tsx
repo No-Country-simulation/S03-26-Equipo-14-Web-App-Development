8084d05a3800041108d8d998903542309a77e790
Gabriela Díaz <gldt2494@gmail.com>
2026-04-05 18:01:29 -0500

    feat: add draft screen controller

--- apps/cms-app/app/(user)/dashboard/page.tsx
+++ apps/cms-app/app/(user)/dashboard/page.tsx
@@ -1,19 +1,28 @@
+'use client';
+
+// import { ErrorMessage } from './_components/error-message';
 import { EmptyDashboard } from './_components/empty-dashboard';
-import { ErrorMessage } from './_components/error-message';
 import { NoProjects } from './_components/no-projects';
 import { Loading } from './_components/loading';
-import { NoResults } from './_components/no-results';
-import { FiltersBar } from './_components/filtersbar';
+import { DashboardView } from './_components/dashboard-view';
+import { useProjectStore } from '@/store/useProjectStore';
+import { testimonials } from '@/store/useProjectStore';
 
 export default function DashboardPage() {
-  return (
-    <section className="flex flex-col gap-4 h-full">
-      <FiltersBar />
-      {/* <EmptyDashboard /> */}
-      <NoResults />
-    </section>
-  );
-  // return <ErrorMessage />;
-  // return <NoProjects />;
-  // return <Loading/>;
+  // const projects = [];
+  const { projects, selectedProjectId } = useProjectStore();
+  // const testimonials = [];
+
+  let content = <Loading />; // Estado inicial de carga
+
+  if (projects.length === 0) {
+    content = <NoProjects />;
+  } else if (testimonials.length === 0) {
+    content = <EmptyDashboard />;
+  } else {
+    content = <DashboardView />;
+  }
+
+  return <section className="flex flex-col gap-4 h-full">{content}</section>;
+  z;
 }
