594717a4f80a8c0f96a136d60701f4ee43b70d32
Gabriela Díaz <gldt2494@gmail.com>
2026-04-05 21:35:47 -0500

    feat: update DashboardView to accept testimonials as props and render TestimonialCard components

--- apps/cms-app/app/(user)/dashboard/_components/dashboard-view.tsx
+++ apps/cms-app/app/(user)/dashboard/_components/dashboard-view.tsx
@@ -1,17 +1,30 @@
 import { FiltersBar } from './filtersbar';
+import { TestimonialCard } from './testimonial-card';
 import { NoResults } from './no-results';
+import { Testimonial } from '@/store/useProjectStore';
 
-export function DashboardView() {
+export function DashboardView({
+  testimonials,
+}: {
+  testimonials: Testimonial[];
+}) {
   return (
     <>
       <FiltersBar />
-      <section className="grid gap-4 [grid-template-columns:repeat(auto-fit,minmax(280px,1fr))]">
-        {/* <div className="bg-blue-500 min-h-[180px]">Card</div>
-        <div className="bg-blue-500 min-h-[180px]">Card</div>
-        <div className="bg-blue-500 min-h-[180px]">Card</div>
-        <div className="bg-blue-500 min-h-[180px]">Card</div>
-        <div className="bg-blue-500 min-h-[180px]">Card</div> */}
+      {/* Si tenemos datos */}
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+        {testimonials.map((testimonial) => (
+          <TestimonialCard
+            key={testimonial.id}
+            type={testimonial.type}
+            status={testimonial.status}
+            author={testimonial.author}
+            description={testimonial.content}
+            updatedAt={testimonial.updatedAt}
+          />
+        ))}
       </section>
+      {/* Si no hay resultados */}
       {/* <NoResults /> */}
     </>
   );
