52c5a1657af8c3d74d0b7682473364c2ca435734
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-10 10:54:07 -0300

    fix: provider repository

--- apps/cms-api/src/app.module.ts
+++ apps/cms-api/src/app.module.ts
@@ -2,28 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { AuthModule } from './api/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { JwtStrategy } from './api/auth/strategies/jwt.strategy';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './api/auth/guards/jwt-auth.guard';
-import {
-  UserRepository,
-  CategoryRepository,
-  TestimonialRepository,
-  PrismaModule,
-  TagRepository,
-  OrganizationMemberRepository,
-  OrganizationRepository,
-} from '@repo/api';
-import { CategoryModule } from './api/category/category.module';
-import { MailModule } from './api/mail/mail.module';
-import { TestimonialsModule } from './api/testimonials/testimonials.module';
-import { TagModule } from './api/tag/tag.module';
-import { OrgRolesGuard } from './api/common/guards/organization-role.guard';
-import { ProjectsModule } from './api/projects/projects.module';
-import { AnalyticsModule } from './api/analytics/analytics.module';
-import { OrganizationModule } from './api/organization/organization.module';
 import { ApiModule } from './api/api.module';
 import { PublicModule } from './public/public.module';
 
--- apps/cms-api/src/public/embed/embed.module.ts
+++ apps/cms-api/src/public/embed/embed.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmbedService } from './embed.service';
 import { EmbedController } from './embed.controller';
+import { EmbedRepository } from '@repo/api';
 
 @Module({
   controllers: [EmbedController],
-  providers: [EmbedService],
+  providers: [EmbedService, EmbedRepository],
 })
 export class EmbedModule {}
