9798487a09acc232c43deea06cc45d2146162d4a
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-04-15 19:01:44 -0300

    feat: added swagger descriptions in embed ep, fix services and controller

--- apps/cms-api/src/public/embed/embed.controller.ts
+++ apps/cms-api/src/public/embed/embed.controller.ts
@@ -1,8 +1,23 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseEnumPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { EmbedService } from './embed.service';
 import { Public } from 'src/api/auth/decorators/public.decorator';
 import { EmbedApiKeyGuard } from './guards/embed-api-key.guard';
 import { EmbedCredentials } from './decorators/embed-credentials.decorator';
+import {
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TestimonialType } from '@repo/api';
 
 @Controller('embed')
 export class EmbedController {
@@ -11,9 +26,33 @@ export class EmbedController {
   @Public()
   @UseGuards(EmbedApiKeyGuard)
   @Get()
+  @ApiOperation({ summary: 'Endpoint with specific header' })
+  @ApiQuery({
+    name: 'type',
+    required: false,
+    description: 'Type for filter data',
+    enum: TestimonialType,
+  })
+  @ApiHeader({
+    name: 'x-embed-key',
+    description: 'ApiKey required',
+    required: true,
+    schema: {
+      type: 'string',
+      default: 'Example cms:api:key',
+    },
+  })
   getTestimonials(
     @EmbedCredentials() credentials: { projectId: string; orgId: string },
+    @Query(
+      'type',
+      new ParseEnumPipe(TestimonialType, {
+        optional: true,
+      }),
+    )
+    type?: TestimonialType,
   ) {
-    return this.embedService.getPublishedTestimonials(credentials);
+    const validType = type !== undefined ? type : '';
+    return this.embedService.getPublishedTestimonials(validType, credentials);
   }
 }
